53a8d1a8ae79413514e811b19c323b5df1ca2c63
Aleksandr Markov <ydmarkov@yandex.ru>
2020-03-07 18:03:35 +0300

    lab5

--- lab4.h
+++ lab4.h
@@ -47,13 +47,64 @@ public:
          return A1 * F(x1) + A2 * F(x2) + A3 * F(x3);
     }
 
+    double Gauss() {
+        double a = 0, b = 1;
+        auto sd = (b - a) / 2;
+        auto ss = (b + a) / 2;
+        auto x1 = -1/sqrt(3), x2 = 1/sqrt(3);
+        return sd * (f(sd * x1 + ss) + f(sd * x2 + ss));
+    }
+
+    double Gassa() {
+        double m0 = 1.3333333333;
+        double m1 = 0.7619047619;
+        double m2 = 0.55411255411;
+        double m3 = 0.44329004329;
+
+        logging->addItem("m0 = " + QString::number(m0, 'g', 10));
+        logging->addItem("m1 = " + QString::number(m1, 'g', 10));
+        logging->addItem("m2 = " + QString::number(m2, 'g', 10));
+        logging->addItem("m3 = " + QString::number(m3, 'g', 10));
+
+        double a1 = (m0 * m3 - m2 * m1) / (m1 * m1 - m2 * m0);
+        double a2 = (m2 * m2 - m3 * m1) / (m1 * m1 - m2 * m0);
+
+        double x1 = (-a1 - sqrt(a1 * a1 - 4 * a2)) / 2;
+        double x2 = (-a1 + sqrt(a1 * a1 - 4 * a2)) / 2;
+
+        logging->addItem("x1 = " + QString::number(x1, 'g', 10));
+        logging->addItem("x2 = " + QString::number(x2, 'g', 10));
+
+        double A1 = (m1 - x2 * m0) / (x1 - x2);
+        double A2 = (m1 - x1 * m0) / (x2 - x1);
+
+        logging->addItem("A1 = " + QString::number(A1, 'g', 10));
+        logging->addItem("A2 = " + QString::number(A2, 'g', 10));
+
+        logging->addItem("A1 + A2 = " + QString::number(A1 + A2, 'g', 10) + " ;  m0 = " + QString::number(m0, 'g', 10));
+
+        return A1 * f(x1) + A2 * f(x2);
+    }
+
     void run() override {
         logging->addItem("Точное значение: 1.0714361983");
+
+        logging->addItem("");
         logging->addItem("Формула средних прямоугольников с 3 узлами: " + QString::number(simpleIntegral(0, 1, 3), 'g', 10));
         logging->addItem("Погрешность: " + QString::number(abs(accurate - simpleIntegral(0, 1, 3)), 'g', 10));
 
+        logging->addItem("");
         logging->addItem("Квадратурная формула с весовой функцией 1/(1 - x)^(1/4): " + QString::number(interpolIntegral(), 'g', 10));
         logging->addItem("Погрешность: " + QString::number(abs(accurate - interpolIntegral()), 'g', 10));
+
+        logging->addItem("");
+        logging->addItem("По формуле Гаусса (2 узла): " + QString::number(Gauss(), 'g', 10));
+        logging->addItem("Погрешность: " + QString::number(abs(accurate - Gauss()), 'g', 10));
+
+        logging->addItem("");
+        double gassa = Gassa();
+        logging->addItem("По формуле типа Гаусса (2 узла): " + QString::number(gassa, 'g', 10));
+        logging->addItem("Погрешность: " + QString::number(abs(gassa - accurate), 'g', 10));
 
     }
 };
